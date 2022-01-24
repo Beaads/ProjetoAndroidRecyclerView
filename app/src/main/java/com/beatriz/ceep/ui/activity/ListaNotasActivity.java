ce01ef4f02f42c018a391e1b4ffee47296f5b3e2
Beaads <biacriciuma@hotmail.com>
2022-01-24 12:01:18 -0300

    Inicio projeto.

---
+++ app/src/main/java/com/beatriz/ceep/ui/activity/ListaNotasActivity.java
@@ -0,0 +1,41 @@
+package com.beatriz.ceep.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.beatriz.ceep.R;
+import com.beatriz.ceep.dao.NotaDAO;
+import com.beatriz.ceep.model.Nota;
+import com.beatriz.ceep.ui.recyclerview.ListaNotasAdapter;
+
+import java.util.List;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+        List<Nota> todasNotas = notasDeExemplo();
+        configuraRecyclerView(todasNotas);
+    }
+
+    private List<Nota> notasDeExemplo() {
+        NotaDAO dao = new NotaDAO();
+        dao.insere(new Nota("Primeira nota", "Descrição pequena"),
+        new Nota("Segunda nota","Segunda descrição é bem maior que a da primeira nota"));
+        return dao.todos();
+    }
+
+    private void configuraRecyclerView(List<Nota> todasNotas) {
+        RecyclerView listaNotas = findViewById(R.id.lista_notas_recyclerview);
+        configuraAdapter(todasNotas, listaNotas);
+    }
+
+    private void configuraAdapter(List<Nota> todasNotas, RecyclerView listaNotas) {
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
+    }
+}
